7ae02a0bd668b734ff7305f434da872a72c0a4a7
123ABm <94301088+123ABm@users.noreply.github.com>
2025-08-08 10:09:48 +0800

    feat:pinia持久化实现

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,11 +1,94 @@
-<script setup lang="ts">
-//
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <view class="index">index</view>
+  <view class="container">
+    <uni-section title="卡片+列表" type="line">
+      <uni-card padding="0" spacing="0">
+        <template v-slot:cover>
+          <view class="custom-cover">
+            <image
+              class="cover-image"
+              mode="aspectFill"
+              src="https://qiniu-web-assets.dcloud.net.cn/unidoc/zh/shuijiao.jpg"
+            >
+            </image>
+            <view class="cover-content">
+              <text class="uni-subtitle uni-white">今日新闻热点</text>
+            </view>
+          </view>
+        </template>
+        <uni-list>
+          <uni-list-item title="今日新闻" showArrow></uni-list-item>
+          <uni-list-item title="今日新闻" showArrow></uni-list-item>
+        </uni-list>
+        <view slot="actions" class="card-actions no-border">
+          <view class="card-actions-item" @click="actionsClick('分享')">
+            <uni-icons type="pengyouquan" size="18" color="#999"></uni-icons>
+            <text class="card-actions-item-text">分享</text>
+          </view>
+          <view class="card-actions-item" @click="actionsClick('点赞')">
+            <uni-icons type="heart" size="18" color="#999"></uni-icons>
+            <text class="card-actions-item-text">点赞</text>
+          </view>
+          <view class="card-actions-item" @click="actionsClick('评论')">
+            <uni-icons type="chatbubble" size="18" color="#999"></uni-icons>
+            <text class="card-actions-item-text">评论</text>
+          </view>
+        </view>
+      </uni-card>
+    </uni-section>
+  </view>
 </template>
 
 <style lang="scss">
-//
+.container {
+  overflow: hidden;
+}
+
+.custom-cover {
+  flex: 1;
+  flex-direction: row;
+  position: relative;
+}
+
+.cover-content {
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  height: 40px;
+  background-color: rgba($color: #000000, $alpha: 0.4);
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  padding-left: 15px;
+  font-size: 14px;
+  color: #fff;
+}
+
+.card-actions {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
+  height: 45px;
+  border-top: 1px #eee solid;
+}
+.card-actions-item {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+}
+.card-actions-item-text {
+  font-size: 12px;
+  color: #666;
+  margin-left: 5px;
+}
+.cover-image {
+  flex: 1;
+  height: 150px;
+}
+.no-border {
+  border-width: 0;
+}
 </style>
--- src/types/component.d.ts
+++ src/types/component.d.ts
@@ -4,8 +4,46 @@
  * declare module 'vue'
  */
 import 'vue'
+
 declare module 'vue' {
   export interface GlobalComponents {
-    //
+    // uni-ui 组件类型声明 - 使用 @uni-helper/uni-ui-types 提供的类型
+    UniCard: import('@uni-helper/uni-ui-types')['UniCard']
+    UniSection: import('@uni-helper/uni-ui-types')['UniSection']
+    UniList: import('@uni-helper/uni-ui-types')['UniList']
+    UniListItem: import('@uni-helper/uni-ui-types')['UniListItem']
+    UniIcons: import('@uni-helper/uni-ui-types')['UniIcons']
+    UniButton: import('@uni-helper/uni-ui-types')['UniButton']
+    UniInput: import('@uni-helper/uni-ui-types')['UniInput']
+    UniPopup: import('@uni-helper/uni-ui-types')['UniPopup']
+    UniSwipeAction: import('@uni-helper/uni-ui-types')['UniSwipeAction']
+    UniSwipeActionItem: import('@uni-helper/uni-ui-types')['UniSwipeActionItem']
+    UniTag: import('@uni-helper/uni-ui-types')['UniTag']
+    UniRate: import('@uni-helper/uni-ui-types')['UniRate']
+    UniNoticeBar: import('@uni-helper/uni-ui-types')['UniNoticeBar']
+    UniNumberBox: import('@uni-helper/uni-ui-types')['UniNumberBox']
+    UniPagination: import('@uni-helper/uni-ui-types')['UniPagination']
+    UniSteps: import('@uni-helper/uni-ui-types')['UniSteps']
+    UniTable: import('@uni-helper/uni-ui-types')['UniTable']
+    UniTh: import('@uni-helper/uni-ui-types')['UniTh']
+    UniTr: import('@uni-helper/uni-ui-types')['UniTr']
+    UniTd: import('@uni-helper/uni-ui-types')['UniTd']
+    UniCalendar: import('@uni-helper/uni-ui-types')['UniCalendar']
+    UniDatetimePicker: import('@uni-helper/uni-ui-types')['UniDatetimePicker']
+    UniDrawer: import('@uni-helper/uni-ui-types')['UniDrawer']
+    UniEasyinput: import('@uni-helper/uni-ui-types')['UniEasyinput']
+    UniFab: import('@uni-helper/uni-ui-types')['UniFab']
+    UniForms: import('@uni-helper/uni-ui-types')['UniForms']
+    UniFormsItem: import('@uni-helper/uni-ui-types')['UniFormsItem']
+    UniGrid: import('@uni-helper/uni-ui-types')['UniGrid']
+    UniGridItem: import('@uni-helper/uni-ui-types')['UniGridItem']
+    UniGroup: import('@uni-helper/uni-ui-types')['UniGroup']
+    UniIndexedList: import('@uni-helper/uni-ui-types')['UniIndexedList']
+    UniLink: import('@uni-helper/uni-ui-types')['UniLink']
+    UniLoadMore: import('@uni-helper/uni-ui-types')['UniLoadMore']
+    UniNavBar: import('@uni-helper/uni-ui-types')['UniNavBar']
+    UniSearchBar: import('@uni-helper/uni-ui-types')['UniSearchBar']
+    UniSegmentedControl: import('@uni-helper/uni-ui-types')['UniSegmentedControl']
+    UniTransition: import('@uni-helper/uni-ui-types')['UniTransition']
   }
 }
